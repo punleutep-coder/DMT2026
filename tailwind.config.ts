bf770519b9fe97a10e35ceba819f56d327725e3b
Punleu Tep <punleu.tep@gmail.com>
2025-08-15 15:07:26 +0000

    Please install Khmer front " KhmerOSbattambang.ttf " to this app in orde

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Roboto', '"Khmer OS Battambang"', 'sans-serif'],
+        headline: ['Roboto', '"Khmer OS Battambang"', 'sans-serif'],
         code: ['Source Code Pro', 'monospace'],
       },
       colors: {
