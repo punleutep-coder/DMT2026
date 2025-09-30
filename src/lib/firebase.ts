63fc826655f1a7572f42591e2f159bcf0c7f6db1
Punleu Tep <punleu.tep@gmail.com>
2025-09-30 02:09:33 +0000

    [2025-09-30T01:50:36.897Z]  @firebase/database: FIREBASE WARNING: Missin

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,4 +17,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize App Check
+if (typeof window !== 'undefined') {
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider('6Ld-9QoqAAAAAGw_85w3_f69fJbH_s5X_ghG-3eD'),
+    isTokenAutoRefreshEnabled: true
+  });
+}
+
 export const db = getDatabase(app);
