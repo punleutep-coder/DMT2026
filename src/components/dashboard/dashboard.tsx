f2490dd0d38f014f1269395cd22097a06e80b1af
Punleu Tep <punleu.tep@gmail.com>
2025-10-17 05:39:45 +0000

    apply html and css to this button, for HTML code "<!-- From Uiverse.io b

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -52,13 +52,28 @@ export default function Dashboard() {
           <DocumentManagement />
         </main>
         {hasPermission(currentUser, 'canAddDocument') && (
-          <Button 
-            onClick={() => openModal('addDocument')} 
-            className="fixed bottom-6 right-6 h-12 w-12 rounded-full bg-primary/80 hover:bg-primary/90 text-[#000066] shadow-2xl hover:shadow-xl transition-all duration-300 z-50 p-0 flex items-center justify-center"
+          <div
+            onClick={() => openModal('addDocument')}
+            className="fixed bottom-6 right-6 z-50 cursor-pointer"
             title={t('addDocument')}
           >
-            <FilePlus className="h-12 w-12" />
-          </Button>
+            <div className="loader">
+              <svg width="100" height="100" viewBox="0 0 100 100">
+                <defs>
+                  <mask id="clipping">
+                    <polygon points="0,0 100,0 100,100 0,100" fill="black"></polygon>
+                    <polygon points="25,25 75,25 50,75" fill="white"></polygon>
+                    <polygon points="50,25 75,75 25,75" fill="white"></polygon>
+                    <polygon points="35,35 65,35 50,65" fill="white"></polygon>
+                    <polygon points="35,35 65,35 50,65" fill="white"></polygon>
+                    <polygon points="35,35 65,35 50,65" fill="white"></polygon>
+                    <polygon points="35,35 65,35 50,65" fill="white"></polygon>
+                  </mask>
+                </defs>
+              </svg>
+              <div className="box"></div>
+            </div>
+          </div>
         )}
       </div>
       {state.dialog.isOpen && <ConfirmDialog />}
